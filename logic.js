ef15e59357596c1809f7ae6cf5d53d3c2f95efa5
Faizan <faizanfaruk8@gmail.com>
2022-06-11 18:38:12 +0530

    burgerking

---
+++ logic.js
@@ -0,0 +1,197 @@
+
+
+const theme = () => {
+
+        if (document.getElementsByClassName('topBar')[0].style.backgroundColor == 'rgb(245, 235, 220)') 
+        {
+           
+            document.getElementById('theimg').src="images/light.png";
+            document.getElementById('theimg').style.transition = "all 0.5s";
+            document.getElementById('theimg').style.borderRadius = "50%";       
+
+             
+
+                
+
+                document.getElementById('w1').style.backgroundColor = '#15202b';
+                document.getElementById('w1').style.transition = 'all 0.5s';
+
+
+                document.getElementById('w2').style.backgroundColor = '#15202b';
+                document.getElementById('w2').style.transition = 'all 0.5s';
+
+                document.getElementById('t1').style.backgroundColor = '#15202b';
+                document.getElementById('t1').style.transition = 'all 0.5s';
+
+                document.getElementById('t2').style.backgroundColor = '#15202b';
+                document.getElementById('t2').style.transition = 'all 0.5s';
+
+                document.getElementById('t3').style.backgroundColor = '#15202b';
+                document.getElementById('t3').style.transition = 'all 0.5s';
+
+                let a = document.getElementsByTagName('a');
+
+                document.body.style.backgroundColor = '#15202b';
+                document.body.style.transition = 'all 0.5s';
+
+
+                a[0].style.color = '#fff';
+                a[0].style.backgroundColor = '#15202b';
+                a[0].style.transition = 'all 0.5s';
+
+                a[1].style.color = '#fff';
+                a[1].style.backgroundColor = '#15202b';
+                a[1].style.transition = 'all 0.5s';
+
+                a[2].style.color = '#fff';
+                a[2].style.backgroundColor = '#15202b';
+                a[2].style.transition = 'all 0.5s';
+
+                a[3].style.color = '#fff';
+                a[3].style.backgroundColor = '#15202b';
+                a[3].style.transition = 'all 0.5s';
+
+
+                a[4].style.color = '#fff';
+                a[4].style.backgroundColor = '#15202b';
+                a[4].style.transition = 'all 0.5s';
+
+
+                a[5].style.color = '#fff';
+                a[5].style.backgroundColor = '#15202b';
+                a[5].style.transition = 'all 0.5s';
+
+
+                let img = document.getElementsByTagName('img');
+
+
+                img[0].style.backgroundColor = '#15202b';
+                img[0].style.transition = 'all 0.5s';
+
+                img[1].style.backgroundColor = '#15202b';
+                img[1].style.transition = 'all 0.5s';
+
+                document.getElementsByClassName('burgerLogo')[0].style.backgroundColor = '#15202b';
+                document.getElementsByClassName('burgerLogo')[0].style.transition = 'all 0.5s';
+
+
+                document.getElementsByClassName('topbarlogo')[0].style.backgroundColor = '#15202b';
+                document.getElementsByClassName('topbarlogo')[0].style.transition = 'all 0.5s';
+
+
+                document.getElementsByClassName('footwrap')[0].style.backgroundColor = '#1c1c1c';
+                document.getElementsByClassName('footwrap')[0].style.transition = 'all 0.5s';
+
+
+                document.getElementsByClassName('ft')[0].style.color = '#fff';
+                document.getElementsByClassName('ft')[0].style.transition = 'all 0.5s';
+
+
+                document.getElementsByClassName('ft')[1].style.color = '#fff';
+                document.getElementsByClassName('ft')[1].style.transition = 'all 0.5s';
+
+
+                document.getElementsByClassName('footdescript')[0].style.color = '#fff';
+                document.getElementsByClassName('footdescript')[0].style.transition = 'all 0.5s';
+
+                
+                document.getElementsByClassName('temp1')[0]. style.boxShadow = '0px 0px 10px #fff';
+                document.getElementsByClassName('temp2')[0]. style.boxShadow = '0px 0px 10px #fff';
+                document.getElementsByClassName('temp3')[0]. style.boxShadow = '0px 0px 10px #fff';
+                document.getElementsByClassName('temp4')[0]. style.boxShadow = '0px 0px 10px #fff';
+
+        }
+
+        else {
+              
+                document.getElementById('theimg').src="images/dark.png";
+                document.getElementById('theimg').style.transition = "all 0.5s";
+                document.getElementById('theimg').style.borderRadius = "50%";  
+
+
+                document.getElementById('w1').style.backgroundColor = 'rgb(245, 235, 220)';
+                document.getElementById('w1').style.transition = 'all 0.5s';
+
+                document.getElementById('w2').style.backgroundColor = 'rgb(245, 235, 220)';
+                document.getElementById('w2').style.transition = 'all 0.5s';
+
+                document.getElementById('t1').style.backgroundColor = 'rgb(245, 235, 220)';
+                document.getElementById('t1').style.transition = 'all 0.5s';
+
+                document.getElementById('t2').style.backgroundColor = 'rgb(245, 235, 220)';
+                document.getElementById('t2').style.transition = 'all 0.5s';
+
+                document.getElementById('t3').style.backgroundColor = 'rgb(245, 235, 220)';
+                document.getElementById('t3').style.transition = 'all 0.5s';
+
+                document.body.style.backgroundColor = 'rgb(245, 235, 220)';
+                document.body.style.transition = 'all 0.5s';
+
+
+                let a = document.getElementsByTagName('a');
+
+
+
+                a[0].style.color = '#502314';
+                a[0].style.backgroundColor = 'rgb(245, 235, 220)';
+                a[0].style.transition = 'all 0.5s';
+
+                a[1].style.color = '#502314';
+                a[1].style.backgroundColor = 'rgb(245, 235, 220)';
+                a[1].style.transition = 'all 0.5s';
+
+                a[2].style.color = '#502314';
+                a[2].style.backgroundColor = 'rgb(245, 235, 220)';
+                a[2].style.transition = 'all 0.5s';
+
+                a[3].style.color = '#502314';
+                a[3].style.backgroundColor = 'rgb(245, 235, 220)';
+                a[3].style.transition = 'all 0.5s';
+
+                a[4].style.color = '#502314';
+                a[4].style.backgroundColor = 'rgb(245, 235, 220)';
+                a[4].style.transition = 'all 0.5s';
+
+                a[5].style.color = '#502314';
+                a[5].style.backgroundColor = 'rgb(245, 235, 220)';
+                a[5].style.transition = 'all 0.5s';
+
+
+                let img = document.getElementsByTagName('img');
+
+
+                img[0].style.backgroundColor = 'rgb(245, 235, 220)';
+                img[0].style.transition = 'all 0.5s';
+
+                img[1].style.backgroundColor = 'rgb(245, 235, 220)';
+                img[1].style.transition = 'all 0.5s';
+
+                document.getElementsByClassName('burgerLogo')[0].style.backgroundColor = 'rgb(245, 235, 220)';
+                document.getElementsByClassName('burgerLogo')[0].style.transition = 'all 0.5s';
+
+
+                document.getElementsByClassName('topbarlogo')[0].style.backgroundColor = 'rgb(245, 235, 220)';
+                document.getElementsByClassName('topbarlogo')[0].style.transition = 'all 0.5s';
+
+                document.getElementsByClassName('footwrap')[0].style.backgroundColor = 'rgb(245, 235, 220)';
+                document.getElementsByClassName('footwrap')[0].style.transition = 'all 0.5s';
+
+
+                document.getElementsByClassName('ft')[0].style.color = 'rgb(80, 35, 20)';
+                document.getElementsByClassName('ft')[0].style.transition = 'all 0.5s';
+
+
+                document.getElementsByClassName('ft')[1].style.color = 'rgb(80, 35, 20)';
+                document.getElementsByClassName('ft')[1].style.transition = 'all 0.5s';
+
+
+                document.getElementsByClassName('footdescript')[0].style.color = 'rgb(80, 35, 20)';
+                document.getElementsByClassName('footdescript')[0].style.transition = 'all 0.5s';
+
+                document.getElementsByClassName('temp1')[0]. style.boxShadow = 'none';
+                document.getElementsByClassName('temp2')[0]. style.boxShadow = 'none';
+                document.getElementsByClassName('temp3')[0]. style.boxShadow = 'none';
+                document.getElementsByClassName('temp4')[0]. style.boxShadow = 'none';
+        }
+
+}
